42985b59da2c4646d5a67b3cf492ddf2c7a39b04
Gino <sahagun.gino@gmail.com>
2017-10-08 20:51:59 -0700

    Open Door Static

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "OpenDoor.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -18,8 +18,13 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	//Create Actor
+	AActor* owner = GetOwner();
+	//Get Access to rotation
+	FQuat rotation = owner->GetTransform().GetRotation();
+	FRotator newRotation = {0.0f, 60.0f, 0.0f};
+	//Set Actor Rotation
+	owner->SetActorRotation(newRotation);
 	
 }
 
