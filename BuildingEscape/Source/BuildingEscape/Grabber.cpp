40dd560298beb83379820d1df80d8e85cecd3a1d
Gino <sahagun.gino@gmail.com>
2017-10-10 20:43:36 -0700

    physics body

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Grabber.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber Reporting for Duty"));
+
+	PlayerController = GetWorld()->GetFirstPlayerController();
+	// ...
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	//Get Player View Point
+	PlayerController->GetPlayerViewPoint(viewOut,rotateOut);
+	//Ray-Cast to Reach distance
+	//UE_LOG(LogTemp, Warning, TEXT("Player View %s , Rotate View %s "),*viewOut.ToString(), *rotateOut.ToString())
+
+	FVector lineTraceEnd = viewOut + rotateOut.Vector() * reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		viewOut,
+		lineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.0f
+	);
+
+
+	//To See what we Hit
+	// ...
+}
+
